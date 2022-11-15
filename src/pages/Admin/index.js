8a43ac164a470e4565af389d8f01c5bac9167737
Diego Carvalho <dev.diegocarvalho@gmail.com>
2022-11-15 16:21:09 -0300

    Buscar, mostrar e deletar itens no banco

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './admin.css'
 import { Header } from '../../components/Header'
 import { Logo } from '../../components/Logo'
@@ -25,6 +25,29 @@ export default function Admin(){
   const [backgroundColorInput, setBackgroundColorInput] = useState("#f1f1f1")
   const [textColorInput, setTextColorInput] = useState("#121212")
 
+  const [links, setLinks] = useState([])
+
+  useEffect(() => {
+    const linksRef = collection(db, 'links')
+    const queryRef = query(linksRef, orderBy('created', 'asc'))
+
+    const unsub = onSnapshot(queryRef, (snapshot) => {
+      let lista = []
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          name: doc.data().name,
+          url: doc.data().url,
+          bg: doc.data().bg,
+          color: doc.data().color
+        })
+      })
+      setLinks(lista)
+    })
+
+  }, [])
+
   async function handleRegister(e){
     e.preventDefault()
 
@@ -49,6 +72,11 @@ export default function Admin(){
       console.log(`Erro ao registrar : ${error}` )
       toast.error("Oops! Erro ao salvar o link!")
     })
+  }
+
+  async function handleDeleteLink(id) {
+    const docRef = doc(db, 'links', id)
+    await deleteDoc(docRef)
   }
 
 
@@ -112,17 +140,20 @@ export default function Admin(){
 
       <h2 className='title'>Meus links</h2>
 
-      <article
-      className='list animate-pop'
-      style = {{ backgroundColor: "#000", color:"#fff" }}            
-      >
-        <p>Grupo no Telegram</p>
-        <div>
-          <button className='btn-delete'>
-            <FiTrash2 size={18} color='#fff'/>
-          </button>
-        </div>
-      </article>
+      { links.map((item, index) => (
+        <article
+        key={index}
+        className='list animate-pop'
+        style = {{ backgroundColor: item.bg, color: item.color }}            
+        >
+          <p>{item.name}</p>
+          <div>
+            <button className='btn-delete' onClick={() => handleDeleteLink(item.id)}>
+              <FiTrash2 size={18} color='#fff'/>
+            </button>
+          </div>
+        </article>
+      ))}
 
     </div>
   )
