2045cf9467e96ae022e0c5aecc9136f804f46d3f
Diego Carvalho <dev.diegocarvalho@gmail.com>
2022-11-17 00:07:58 -0300

    cadastro de rotas, redes e links sociais no banco de dados

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Admin from './pages/Admin'
+import Networks from './pages/Networks'
 import Error from './pages/Error'
 import Private from './routes/Private'
 
@@ -18,6 +19,10 @@ const router =createBrowserRouter([
   {
     path: '/admin',
     element: <Private> <Admin/> </Private>
+  },
+  {
+    path: '/admin/social',
+    element: <Private> <Networks/> </Private>
   },
   {
     path: '*',
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,106 @@
+import { useState, useEffect } from 'react'
+
 import './home.css'
 import { Social } from '../../components/Social'
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
 
+import {
+  getDocs,
+  collection,
+  orderBy,
+  query,
+  doc,
+  getDoc
+} from 'firebase/firestore'
+
+import { db } from '../../services/firebaseConnection'
+
 export default function Home() {
+
+  const [links, setLinks] = useState([])
+  const [socialLinks, setSocialLinks] = useState({})
+
+  useEffect(() => {
+    function loadLinks() {
+      const linksRef = collection(db, 'links')
+      const queryRef = query(linksRef, orderBy('created', 'asc'))
+      
+      getDocs(queryRef)
+      .then( (snapshot) => {
+        let lista = []
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            name: doc.data().name,
+            url: doc.data().url,
+            bg: doc.data().bg,
+            color: doc.data().color
+          })
+        })
+
+        setLinks(lista)
+
+      })      
+      
+    }
+
+    loadLinks()
+  }, [])
+
+  useEffect(() => {
+    function loadSocialLinks(){
+      const docRef = doc(db, 'social', 'link')
+
+      getDoc(docRef)
+      .then((snapshot) => {
+        
+        if(snapshot.data() !== undefined) {
+          setSocialLinks({
+            facebook: snapshot.data().facebook,
+            instagram: snapshot.data().instagram,
+            youtube: snapshot.data().youtube
+          })
+        }
+      })
+    }
+
+    loadSocialLinks()
+  }, [])
+
   return(
     <div className='home-container'>
       <h1>Diego Carvalho</h1>
       <span>Meus Links 👇</span>
 
       <main className='links'>
-      <section className='link-area'>
-        <a href='#'>
-          <p className='link-text'>Canal no Youtube</p>
+      
+      { links.map((item) => (
+        <section key={item.id} className='link-area' style={{ backgroundColor: item.bg }}>
+        <a href={item.url} target='_blank' rel="noreferrer">
+          <p className='link-text' style={{ color: item.color }}>
+              {item.name}
+            </p>
         </a>
       </section>
+      ))}
 
-      <section className='link-area'>
-        <a href='#'>
-          <p className='link-text'>Grupo no Telegram</p>
-        </a>
-      </section>
+      {links.length !== 0 && Object.keys(socialLinks).length > 0 && (
+        <footer>
+            <Social url={socialLinks?.facebook}>
+              <FaFacebook size={35} color="#fff"/>
+            </Social>
 
-      <section className='link-area'>
-        <a href='#'>
-          <p className='link-text'>Facebook</p>
-        </a>
-      </section>
+            <Social url={socialLinks?.instagram}>
+              <FaInstagram size={35} color="#fff"/>
+            </Social>
 
-      <footer>
-        <Social url='https://www.facebook.com/diego.carvalho.73594/'>
-          <FaFacebook size={35} color="#fff"/>
-        </Social>
-
-        <Social url='https://www.instagram.com/diegocarvalhoros/'>
-          <FaInstagram size={35} color="#fff"/>
-        </Social>
-
-        <Social url='https://www.youtube.com/channel/UC3W_VJrxWFcT64ou8OzjGOg'>
-          <FaYoutube size={35} color="#fff"/>
-        </Social>
+            <Social url={socialLinks?.youtube}>
+              <FaYoutube size={35} color="#fff"/>
+            </Social>
       </footer>
+      )}
+
       </main>
      
     </div>
